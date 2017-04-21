7d119f80c97e6f64b6de5d8af801e3d4ab716ca0
Vinicius Z <vini.zama.28@gmail.com>
2017-04-21 19:26:03 -0300

    add my profile

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :list
+  scope :aberto, -> (closed) {where closed: closed}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 	get "lists/index"
 	root "lists#index"
+	get "users/:id" => "users#show"
   resources :tasks
   resources :lists
   devise_for :users
+  resources :users, :only => [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
